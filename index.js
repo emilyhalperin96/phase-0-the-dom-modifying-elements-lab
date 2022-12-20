6fce9b5279e862568f96c930fd0fc161b218d634
emilyphalperin@gmail.com <emilyhalperin@Emilys-MBP.lan>
2022-12-20 12:46:08 -0500

    commit tree 9832748c94ad0064e9edc75fb6d301ca473300fb

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const main = document.getElementById("main");
+main.remove();
+const h1 = document.createElement("h1");
+h1.id = "victory";
+h1.innerHTML = "YOUR_NAME is the champion";
+document.body.appendChild(h1);
+
+const newHeader = document.getElementById("victory");
