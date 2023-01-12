633453a58769669cf7cca0c8f8199be4235103f5
emilyphalperin@gmail.com <emilyhalperin@Emilys-MacBook-Pro-2.local>
2023-01-12 14:22:46 -0500

    finished lab

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+const main = document.getElementById('main');
 main.remove();
+
 const newHeader = document.createElement('h1');
-newHeader.id = "victory"
-newHeader.innerHTML = "Emily is the champion";
+newHeader.id = 'victory'
+newHeader.textContent = "Emily is the champion"
+
+const body = document.querySelector('body');
+body.append(newHeader);
+//not a string because you are passing the element 
+
+//add text to newHeader 
+// '"youre name is the champion"
+// "add in your name in place"
+
